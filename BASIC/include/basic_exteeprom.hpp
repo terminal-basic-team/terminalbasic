d5ffefd67a8b1735878f0b5502831713ef4b1a17
Andrey Skvortsov <starling13@mail.ru>
2017-04-18 15:50:44 +0300

    Add missing files

---
+++ BASIC/include/basic_exteeprom.hpp
@@ -0,0 +1,43 @@
+/*
+ * Terminal-BASIC is a lightweight BASIC-like language interpreter
+ * Copyright (C) 2016, 2017 Andrey V. Skvortsov <starling13@mail.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file basic_exteeprom.hpp
+ * @brief I2C/SPI external eeprom functions and commands
+ */
+
+#ifndef BASIC_EXTEEPROM_HPP
+#define BASIC_EXTEEPROM_HPP
+
+#include "basic_functionblock.hpp"
+
+namespace BASIC
+{
+
+class ExtEEPROM : public FunctionBlock
+{
+public:
+	
+// FunctionBlock interface
+public:
+	void _init() override;
+};
+
+}
+
+#endif
