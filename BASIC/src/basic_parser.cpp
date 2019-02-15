a60b675a907d9b2de1c7900328a6d4c9c60928d6
Andrey Skvortsov <starling13@mail.ru>
2019-02-16 00:05:27 +0300

    Add peek and poke functions

--- BASIC/src/basic_parser.cpp
+++ BASIC/src/basic_parser.cpp
@@ -337,6 +337,12 @@ Parser::fOperator()
 			_lexer.getNext();
 	}
 		break;
+#if USE_PEEK_POKE
+	case Token::KW_POKE:
+		if (!_lexer.getNext() || !fPoke())
+			return false;
+		break;
+#endif
 	case Token::KW_PRINT:
 		if (_lexer.getNext()) {
 			if (!fPrintList())
@@ -717,6 +723,21 @@ Parser::fFnexec(Value &v)
 }
 
 #endif // USE_DEFFN
+
+#if USE_PEEK_POKE
+bool
+Parser::fPoke()
+{
+	Parser::Value v, v2;
+	if (fExpression(v) && (_lexer.getToken() == Token::COMMA) &&
+	 _lexer.getNext() && fExpression(v2)) {
+		if (_mode == EXECUTE)
+			_interpreter.poke(Integer(v), Integer(v2));
+		return true;
+	}
+	return false;
+}
+#endif
 
 /*
  * IMPLICIT_ASSIGNMENT =
