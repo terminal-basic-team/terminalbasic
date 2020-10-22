72de52706888ba050965dfc7c84b468dfa25e912
starling13 <starling13@mail.ru>
2020-10-22 11:51:24 +0300

    Add missing file

---
+++ BASIC/include/HAL_linux.h
@@ -0,0 +1,42 @@
+/*
+ * Terminal-BASIC is a lightweight BASIC-like language interpreter
+ * 
+ * Copyright (C) 2016-2018 Andrey V. Skvortsov <starling13@mail.ru>
+ * Copyright (C) 2019,2020 Terminal-BASIC team
+ *     <https://bitbucket.org/%7Bf50d6fee-8627-4ce4-848d-829168eedae5%7D/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @brief Configuration for the Linux HAL implementation
+ */
+
+#ifndef HAL_LINUX_H
+#define HAL_LINUX_H
+
+#include <sys/cdefs.h>
+
+#define FILES_PATH "/terminal_basic_HAL/"
+
+#if HAL_NVRAM
+#define NVRAM_FILE "nvram.img"
+#endif
+
+#if HAL_EXTMEM
+#define EXTMEM_NUM_FILES 8
+#define EXTMEM_DIR_PATH "extmem/"
+#endif /* HAL_EXTMEM */
+
+#endif /* HAL_LINUX_H */
